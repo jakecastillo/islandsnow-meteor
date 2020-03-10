953e489cfa13562aa23b11b4495ca41040005b56
jakecastillo <jakecast@hawaii.edu>
2020-03-09 15:13:16 -1000

    Done

---
+++ app/imports/ui/components/IslandSnowLogo.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Image } from 'semantic-ui-react';
+
+export default class IslandSnowLogo extends React.Component {
+  render() {
+    return (
+        <Image centered src="http://courses.ics.hawaii.edu/ics314s20/morea/ui-frameworks/experience-islandsnow-logo.png"/>
+    )
+  }
+}
